cad2568fa96fcd2722b6a26dccead299b7645506
Ahmed Walid <34550324+ahmed605@users.noreply.github.com>
2020-04-08 16:52:22 +0200

    dump version to 0.8.56

--- SRC/SparkIV/Properties/AssemblyInfo.cs
+++ SRC/SparkIV/Properties/AssemblyInfo.cs
@@ -27,9 +27,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SparkIV")]
 [assembly: AssemblyDescription("IMG/RPF Archive Viewer/Editor for GTA IV & EFLC")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ahmed")]
+[assembly: AssemblyCompany("Ahmed Walid")]
 [assembly: AssemblyProduct("SparkIV Beta")]
-[assembly: AssemblyCopyright("2008-2019")]
+[assembly: AssemblyCopyright("2008-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.1.0")]
-[assembly: AssemblyFileVersion("0.8.1.0")]
+[assembly: AssemblyVersion("0.8.56.0")]
+[assembly: AssemblyFileVersion("0.8.56.0")]
